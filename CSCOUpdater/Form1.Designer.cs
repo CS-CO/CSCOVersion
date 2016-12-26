2c51e3c42c52f97f637353e112b05ce8228f9969
Tatjam <tajampi@gmail.com>
2016-12-26 16:03:37 +0100

    First commit

---
+++ CSCOUpdater/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CSCOUpdater
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.installed_v = new System.Windows.Forms.Label();
+			this.latest_v = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 46);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(313, 51);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Update";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// installed_v
+			// 
+			this.installed_v.AutoSize = true;
+			this.installed_v.Location = new System.Drawing.Point(9, 9);
+			this.installed_v.Name = "installed_v";
+			this.installed_v.Size = new System.Drawing.Size(116, 17);
+			this.installed_v.TabIndex = 1;
+			this.installed_v.Text = "Installed Version:";
+			this.installed_v.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// latest_v
+			// 
+			this.latest_v.AutoSize = true;
+			this.latest_v.Location = new System.Drawing.Point(9, 26);
+			this.latest_v.Name = "latest_v";
+			this.latest_v.Size = new System.Drawing.Size(103, 17);
+			this.latest_v.TabIndex = 2;
+			this.latest_v.Text = "Latest Version:";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(15, 103);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(310, 23);
+			this.progressBar1.TabIndex = 3;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 50;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.label1.Location = new System.Drawing.Point(12, 129);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(298, 17);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Updater by Tatjam. CS:CO by ZooL and Dayik";
+			this.label1.Click += new System.EventHandler(this.label1_Click_1);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(330, 155);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.latest_v);
+			this.Controls.Add(this.installed_v);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "CS:CO Autoupdater";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label installed_v;
+		private System.Windows.Forms.Label latest_v;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ CSCOUpdater/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+using CG.Web.MegaApiClient;
+
+namespace CSCOUpdater
+{
+	public partial class Form1 : Form
+	{
+		string v_url = "https://raw.githubusercontent.com/tatjam/CSCOVersion/master/VERSION";
+
+		int latest_version = 0;
+		int installed_version = 0;
+
+		bool no_v_file = false;
+
+		bool done = false;
+
+		Task task;
+
+		string latest_url = "";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			IProgress<double> progress = new Progress<double>(b => progressBar1.Value = (int)b);
+			if (!done)
+			{
+				if(!button1.Enabled)
+				{
+
+				}
+				else
+				{
+					button1.Text = "Downloading...";
+
+					delete_dir("./TempDownloadCSCO");
+
+					Directory.CreateDirectory("./TempDownloadCSCO");
+
+					MegaApiClient mega_client = new MegaApiClient();
+					mega_client.LoginAnonymous();
+
+					try
+					{
+						task = mega_client.DownloadFileAsync(new Uri(latest_url), "./TempDownloadCSCO/temp.zip", progress);
+					}
+					catch(Exception ee)
+					{
+						MessageBox.Show("Couldn't Download: " + ee.Message);
+						done = true;
+					}
+				}
+			}
+			else
+			{
+				button1.Text = "Already ran updater!";
+				button1.Enabled = false;
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string v_file = "NULL";
+			string local_v_file = "NULL";
+
+			if (Directory.GetParent("./").Name == "sourcemods")
+			{
+
+
+				button1.Text = "Getting version file";
+				button1.Enabled = false;
+
+
+
+				try
+				{
+					using (System.Net.WebClient client = new System.Net.WebClient())
+					{
+						v_file = client.DownloadString(v_url);
+					}
+				}
+				catch (Exception)
+				{
+					MessageBox.Show("Error! Unable to get version file from github.\nCheck your internet connection!");
+				}
+
+				if(v_file != "NULL")
+				{
+					//Process v_file
+					latest_version = 0;
+
+					string buff = "";
+
+					foreach(char c in v_file)
+					{
+						if (c == ' ')
+						{
+							break;
+						}
+						else
+						{
+							buff += c;
+						}
+					}
+
+					latest_version = Convert.ToInt32(buff);
+
+					buff = "";
+
+					bool reading = false;
+
+					foreach (char c in v_file)
+					{
+						if (!reading)
+						{
+							if (c == ' ')
+							{
+								reading = true;
+							}
+						}
+						else
+						{
+							buff += c;
+						}
+					}
+
+					latest_url = buff;
+				}
+
+				if (!Directory.Exists("./csco"))
+				{
+					MessageBox.Show("No CS:CO installation found. Updating will download last version!");
+				}
+				else
+				{
+
+					try
+					{
+						// Get actual installed version
+						local_v_file = File.ReadAllText("./csco/version.txt");
+						installed_version = Convert.ToInt32(local_v_file);
+					}
+					catch (Exception)
+					{
+
+						MessageBox.Show("Couldn't get version.txt, assuming old version installed. Updating will download and create version.txt");
+						no_v_file = true;
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("You are running the autoupdater from the wrong location. Can't update!");
+			}
+
+			installed_v.Text = "Installed version: " + installed_version;
+			latest_v.Text = "Latest version: " + latest_version;
+
+			if (latest_url != "")
+			{
+				if(installed_version < latest_version)
+				{
+					button1.Text = "Update";
+					button1.Enabled = true;
+				}
+				else
+				{
+					button1.Text = "No Update Required";
+					button1.Enabled = false;
+				}
+
+			}
+			else
+			{
+				button1.Enabled = false;
+				button1.Text = "Can't Update!";
+			}
+		
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			progressBar1.Maximum = 100;
+
+			try
+			{
+				if (task != null)
+				{
+					if (task.IsCompleted)
+					{
+						if (!done)
+						{
+							done = true;
+							progressBar1.Value = 100;
+
+							button1.Text = "Deleting Old Install...";
+
+							if (File.Exists("./HostMe.txt"))
+							{
+								File.Delete("./HostMe.txt");
+							}
+							if (File.Exists("./ReadMe.txt"))
+							{
+								File.Delete("./ReadMe.txt");
+							}
+
+							delete_dir("./csco");
+							button1.Text = "Installing...";
+
+							try
+							{
+								// Extract .zip
+								ZipFile.ExtractToDirectory("./TempDownloadCSCO/temp.zip", "./");
+							}
+							catch (Exception ee)
+							{
+								MessageBox.Show("Couldn't Unzip: " + ee.Message);
+								done = true;
+							}
+
+							if (no_v_file == true || !File.Exists("./csco/version.txt"))
+							{
+								File.WriteAllText("./csco/version.txt", "" + latest_version);
+							}
+
+							// Delete temp files
+							delete_dir("./TempDownloadCSCO");
+
+
+							button1.Enabled = false;
+							button1.Text = "Install Complete";
+						}
+					}
+				}
+			}
+			catch(Exception ee)
+			{
+				MessageBox.Show(ee.Message);
+			}
+		}
+
+		public void delete_dir(string path)
+		{
+			if (Directory.Exists(path))
+			{
+
+				System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+				foreach (FileInfo file in di.GetFiles())
+				{
+					file.Delete();
+				}
+				foreach (DirectoryInfo dir in di.GetDirectories())
+				{
+					dir.Delete(true);
+				}
+
+				Directory.Delete(path);
+			}
+		}
+
+		private void label1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+}
